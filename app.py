97abf04004919f2f75b343347ee9b43a76514d83
farfuraaa <131926345+farfuraaa@users.noreply.github.com>
2023-08-26 23:39:49 +0300

    app.py

--- app.py
+++ app.py
@@ -31,13 +31,7 @@ local_css("style/style.css")
 # ---- HEADER SECTION ----
 with st.container():
     st.subheader("Hi, I am Sven :wave:")
-    st.title("A Data Analyst From Germany")
-    st.write(
-        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
-    )
-    st.write("[Learn More >](https://pythonandvba.com)")
-
-
+   
 # import streamlit as st
 
 # def load_lottieurl(url):
